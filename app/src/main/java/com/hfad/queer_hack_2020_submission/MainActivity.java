a5e4c47d7b5f3f30eb5e23cda682ac4b44c13434
Aaron Schwartz <ams1347@uncw.edu>
2020-12-05 16:16:14 -0500

    Dog image commit

--- app/src/main/java/com/hfad/queer_hack_2020_submission/MainActivity.java
+++ app/src/main/java/com/hfad/queer_hack_2020_submission/MainActivity.java
@@ -3,6 +3,8 @@ package com.hfad.queer_hack_2020_submission;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView coolText = findViewById(R.id.image_caption);
+        coolText.setText("Hello again");
+
+
     }
 }
